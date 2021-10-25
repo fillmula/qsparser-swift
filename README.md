1d0f1d7d45fb73299d6ffd894ae462a35b27026c
Ben Wong <ben.wong@fillmula.com>
2021-10-25 11:14:02 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qsparser
+
+A description of this package.
---
+++ Tests/qsparserTests/qsparserTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import qsparser
+
+final class qsparserTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(qsparser().text, "Hello, World!")
+    }
+}
